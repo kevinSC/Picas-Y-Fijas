71e43551f20e52e325c577f57bfc794cf7aa8e32
KevinSC <kevineitor22@gmail.com>
2018-04-04 00:47:33 -0500

    the method makeAttempt was created in order to emulate an attempt when someone is playing "Picas y Fijas"

--- stadistics/PFgenerator.py
+++ stadistics/PFgenerator.py
@@ -1,8 +1,20 @@
 #! /usr/bin/python
 class PFgenerator:
 
-	def __init__(self, len, restricitions):
+	def __init__ (self, len, restricitions):
 		self.__len = len
 		self.__restricitons = restricitions
 
-hola = 
+	def makeAttempt (self):
+		import random
+		attempt = []
+		for x in range(self.__len):
+			x = random.randint(0, 9) 
+			while (x in self.__restricitons) or (x in attempt):
+				x = random.randint(0, 9)
+			attempt.append(x)	
+		return tuple(attempt)
+
+firstattempt = PFgenerator(4, (1, 2))
+x = firstattempt.makeAttempt()
+print (x)
